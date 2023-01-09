fdcb674d408744e0300dc483335054c123cfc97c
LAPTOP-AAMKC974\ASUS <s109321026@mail1.ncnu.edu.tw>
2023-01-09 15:47:46 +0800

    start now

--- server1.py
+++ server1.py
@@ -2,42 +2,41 @@
 import asyncio
 import websockets
 clients = []  # to store all connected cleints
+lockSign = 0
+owner = None
 
 # handler for socket message activities
 
 
 async def handler(websocket, path):
+    global lockSign, owner
     # print(path) #path is not used currently
+    userName = 'unknown'
     if websocket not in clients:
-        handle = [websocket, 'unknown']
-        clients.append(handle)
-        userName = 'unknown'
-        # clients.append(websocket)  # append new cleint to the array
+        clients.append(websocket)  # append new cleint to the array
 
-    else:
-        userName = getClientName(websocket)
     async for message in websocket:
         print(message, 'received from client')  # print to console
         msg = message.split("###")
         if msg[0] == 'LOGIN':
-            setClientName(websocket, msg[1])  # 當作特別的事
-            print("set client name ", msg[1])
-        else:
-            brocast(message)  # send message to all clents
-
-
-def getClientName(obj):
-    for h in clients:
-        if h[0] == obj:
-            return h[1]
-    return 'unkown'
-
-
-def setClientName(sock, nickname):
-    for h in clients:
-        if h[0] == sock:
-            h[1] = nickname
-            return
+            userName = msg[1]
+            print("set client name,", msg[1])
+        elif message == 'GET':  # 想搶答
+            print("GET", lockSign)
+            if lockSign > 0:  # 被別人搶走
+                await websocket.send('No')
+            else:
+                lockSign = 1
+                owner = websocket  # 我是擁有者
+                await websocket.send('YES')
+        elif message == "DROP":
+            if websocket == owner:  # 是否為搶到的人
+                lockSign = 0
+                await websocket.send('RELEASED')
+            else:
+                await websocket.send('You can not do that')
+        else:  # 一般訊息，廣播出去
+            await brocast(userName+"###"+message)
 
 
 async def brocast(msg):
@@ -59,6 +58,6 @@ asyncio.set_event_loop(loop)  # set the event loop to asyncio
 
 loop.run_until_complete(
     # setup the websocket service and handler
-    websockets.serve(handler, 'localhost', 4545)
+    websockets.serve(handler, '', 4545)
 )  # hook to localhost:4545
 loop.run_forever()  # keep it running
